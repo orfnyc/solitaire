1eca27fc364c7497996099e9500fcf4f1e35fa41
orfnyc <ollie.r.fordham@gmail.com>
2025-10-12 20:18:49 -0400

    Code skeletons

---
+++ src/game/Card.js
@@ -0,0 +1,35 @@
+export default class Card 
+{
+    // # here means the variables are private
+    // That means they can't be directly accessed by other classes
+    #suit;
+    #value;
+
+    /**
+     * Constructs a new Card object
+     * 
+     * @param {String} suit - Spades, Clubs, Hearts, or Diamonds
+     * @param {Number} value - 1-13 where 1 = Ace, 11 = Jack, 12 = Queen, and 13 = King
+     */
+    constructor(suit, value)
+    {
+
+
+    }
+
+    /**
+     * Getter for the Card's suit
+     */
+    get suit()
+    {
+  
+    }
+    
+    /**
+     * Getter for the Card's value
+     */
+    get value()
+    {
+
+    }
+}
---
+++ src/game/CardStack.js
@@ -0,0 +1,59 @@
+export default class CardStack
+{
+    #cards
+    #num_visible
+
+    /**
+     * Constructor for a CardStack
+     */
+    contructor()
+    {
+        this.cards = [];
+        this.num_visible = 0;
+    }
+
+    /**
+     * Removes the top-most card of the card stack and returns it. 
+     * Decrements the number of visible cards by one to account for the 
+     * removed card.
+     * If this would result in no cards being visible in the stack,
+     * make the next topmost card visible.
+     * @returns {Card} returns the first element in cards.
+     */
+    removeTop()
+    {
+
+    }
+
+    /**
+     * Removes teh top-most card of the card stack.
+     * @returns {Card} returns the first element in cards. 
+     */
+    getTop()
+    {
+
+    }
+
+    /**
+     * Checks if a card is valid to add to the card stack, by solitaire rules.
+     * A card is valid to add if it's value is one less than the top-most card
+     * and its color is different from the top-most card's color.
+     * If the card stack is currently empty, only Kings are valid to add.
+     * @param {Card} card  the card to be added.
+     * @returns {boolean} true if card can be added, false if it cannot.
+     */
+    validToAdd(card)
+    {
+
+    }
+
+    /**
+     * Adds a card to the top of the card stack. Increments number of visible
+     * cards by one to include the added card.
+     * @param {Card} card the card to be added
+     */
+    add(card)
+    {
+
+    }
+}
